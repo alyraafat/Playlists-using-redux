28941a2a9f1879aba0a0ec179ef7a161130c9d29
aly raafat <alewaraafat@gmail.com>
2023-02-11 20:45:07 +0200

    done

---
+++ src/store/slices/moviesSlice.js
@@ -0,0 +1,23 @@
+const { createSlice } = require("@reduxjs/toolkit");
+import { reset } from "../actions";
+const moviesSlice = createSlice({
+    name:"movie",
+    initialState:[],
+    reducers:{
+        addMovie(state,action){
+            state.push(action.payload);
+        },
+        removeMovie(state,action){
+            const index = state.indexOf(action.payload);
+            state.splice(index,1)
+        },
+        extraReducers(builder){
+            builder.addCase(reset,(action,state)=>{
+                return [];
+            })
+        }
+    }
+});
+
+export const { addMovie, removeMovie } = moviesSlice.actions;
+export const moviesReducer = moviesSlice.reducer;
